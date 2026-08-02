be5ad6cff9147de9739a3726a4e281b6541b0f06
gcalacdayuniv <gcalacday.univ@gmail.com>
2026-08-02 17:17:58 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // js/app.js
 import { AppRouter } from './router.js';
+import { AuthModule } from './auth.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    AuthModule.init();
     AppRouter.init();
 });
