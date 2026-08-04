2ec0d0a7065c7c1a87bb99eada9c352fde1f4e7f
gcalacdayuniv <gcalacday.univ@gmail.com>
2026-08-05 06:44:25 +0800

    Update components.js

--- js/components.js
+++ js/components.js
@@ -177,7 +177,7 @@ export const Components = {
         const createCourseBtn = user.role.toLowerCase() === 'lecturer' ? `
             <div class="mt-6">
                 <button id="openCreateCourseModalBtn" class="w-full flex justify-center py-2 px-4 border border-blue-300 rounded-md shadow-sm text-sm font-bold text-blue-700 bg-blue-50 hover:bg-blue-100 focus:outline-none transition-colors">
-                    <i class="fa-solid fa-folder-plus mr-2 mt-0.5"></i> Create New Course Module
+                    <i class="fa-solid fa-folder-plus mr-2 mt-0.5"></i> Create New Course
                 </button>
             </div>
         ` : '';
