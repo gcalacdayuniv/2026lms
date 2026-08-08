4504154795d59c6540c7a6166a4ccb4f8bffd5cc
gcalacdayuniv <gcalacday.univ@gmail.com>
2026-08-08 20:41:49 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@
 import { AppRouter } from './router.js';
 import { AuthModule } from './auth.js';
 import { CourseModule } from './course.js';
+import { AppState } from './globals.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    AppState.init();
     AuthModule.init();
     CourseModule.init();
     AppRouter.init();
